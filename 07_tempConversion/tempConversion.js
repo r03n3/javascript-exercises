025ef852a15eb297cd6bf19ca596183e961d4974
r03n3 <r03n3@web.de>
2023-03-09 16:16:25 +0100

    typeconversion to Number to suit the testcasequery

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(temp) {
 let tempConvert=(temp-32)*5/9;
-return tempConvert.toFixed(1);
+return Number(tempConvert.toFixed(1));
 };
 
 const convertToFahrenheit = function(temp) {
 let tempConvert=(temp*9/5+32);
-return tempConvert.toFixed(1);
+return Number(tempConvert.toFixed(1));
 };
 
 // Do not edit below this line
