96fbbbaaefa71294ec34a71f48b82070d889f24c
r03n3 <r03n3@web.de>
2023-03-09 12:01:04 +0100

    filling the function so testing becomes available

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, amount) {
+    if (amount <0) return "ERROR";
+    let string="";
+    for (let i =0; i<amount;i++){
+        string += word ;
+    }
+    return string;
 };
 
 // Do not edit below this line
