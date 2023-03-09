e26934b2c717ab7b5c3c9ed6116caeb6db14f712
r03n3 <r03n3@web.de>
2023-03-09 15:45:36 +0100

    implement a if to proof if the parameters are numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,28 +2,29 @@ const sumAll = function(numberA, numberB) {
  let numberC = 0;
  let numberD = 0;
  let sum = 0;
- if(numberA > numberB){
+ if(!Number.isInteger(numberA) || !Number.isInteger(numberB)) return "ERROR";
+ if(numberA > numberB && numberA>=0 && numberB>=0){
     numberC = Number(numberB);
     numberD = Number(numberA);
- } else if (numberA<numberB){
+     } else if (numberA<numberB && numberA>=0 && numberB>=0){
     numberC = Number(numberA);
     numberD = Number(numberB);
+     }
+     else if (Number(numberA) <0 || Number(numberB) <0  || numberA==numberB){
+        return "ERROR";
+     }
+     else{
+        return "ERROR";}
+     for(let i=0; i<=numberD-numberC;i++){
+        if(i==0){
+            sum+=numberC;
+        }
+        else{
+            sum+=numberC+i;
+        }
+     }
+     return sum;
  }
- else if (numberA <0 || numberB <0  || numberA==numberB){
-    return "ERROR";
- }
- else{
-    return "ERROR";}
- for(let i=0; i<numberD-numberC;i++){
-    if(i=0){
-        sum+=numberC;
-    }
-    else{
-        sum+=numberC+i;
-    }
- }
- return sum;
- } 
 ;
 
 // Do not edit below this line
