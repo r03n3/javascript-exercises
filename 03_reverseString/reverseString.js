3d4d40fd382461e8bd2e24d09c3f2598d1b36169
r03n3 <r03n3@web.de>
2023-03-09 12:16:03 +0100

    writing function to reverse and return the reversed string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    reverseWordArr = text.split ("");
+    let mapReverse = reverseWordArr.map(word => word.split("").reverse().join(""));
+    return mapReverse;
 };
 
 // Do not edit below this line
