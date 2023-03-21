b72add58c6250bdcfc72558057422119559b8374
r03n3 <r03n3@web.de>
2023-03-21 16:55:18 +0100

    .

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(perArr) {
+    let oldest = perArr.reduce(function(prev, curr) {
+        const prevAge = calcAge(prev.yearOfBirth, prev.yearOfDeath);
+        const currAge = calcAge(curr.yearOfBirth, curr.yearOfDeath);
+        return prevAge > currAge ? prev : curr;
+    });
 
+    return oldest;
 };
+
+const calcAge = function(birth, death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death-birth;
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
