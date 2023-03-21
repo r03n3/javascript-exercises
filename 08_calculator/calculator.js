a5dc5e8af31622a3892d043a6ff50f46834ea503
r03n3 <r03n3@web.de>
2023-03-21 11:50:45 +0100

    implement calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	let result = number1 + number2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	let result = number1 - number2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+	let result = numArr.forEach(element => {
+    sum+=element;
+  });
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  let result=1;
+  for(let i =0; i<numArr.length; i++){
+    result *= numArr[i];
+  }
 };
 
-const power = function() {
-	
+const power = function(number, exponent) {
+	let result=number;
+  if(exponent=0){
+    result==1;
+  }
+  else{
+    for (let i=0; i<exponent; i++){
+      result*=number;
+    }
+  }  
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(factor) {
+  let result=1;
+  if(factor==0 || factor==1){
+    result = 1;
+  }
+  else{
+    for(let i=1; i<=factor; i++){
+      result *= i;  
+    }
+  }	
 };
 
 // Do not edit below this line
